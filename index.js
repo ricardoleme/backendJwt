5f0eb4b6d5c96a043026f42f4b670ab50b68983f
Ricardo Leme <ricardo.leme@gmail.com>
2020-09-25 16:05:43 -0300

    Ajustes no CORS

--- index.js
+++ index.js
@@ -19,16 +19,12 @@ const PORT = process.env.PORT || 4000;
 
 // Exemplo de Middleware 
 app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', "*");
-  res.header('Access-Control-Allow-Headers', "*");
- /*
    // Em produção, remova o '*' e atualize com o domínio do seu app
   res.setHeader("Access-Control-Allow-Origin", '*');
   // Cabeçalhos que serão permitidos
   res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token");
   // Métodos que serão permitidos
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  */
   next();  
 });
 
@@ -37,7 +33,7 @@ app.use(bodyParser.json())
 
 
 app.get("/", (req, res) => {
-  res.json({ mensagem: "👏 API 100% funcional!", versao: "1.1.06" });
+  res.json({ mensagem: "👏 API 100% funcional!", versao: "1.1.07" });
 });
 
 /**
